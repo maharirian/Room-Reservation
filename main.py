632ff784ae4c076640c90ab955173100c9cc004d
mah <ma.haririan1383@gmail.com>
2026-02-10 13:34:15 +0330

    add user or admin

--- main.py
+++ main.py
@@ -3,8 +3,29 @@ reserv_list = []
 admin_list = {"admin":"abcd"}
 user_list = {}
 
+def add_member():
+    key = input("add user or admin:")
+    if key =="admin":
+        name = input("enter name:") 
+        pas = input("enter pasword:")
+        admin_list[name] = pas
+        print("admin successfully added")
+    elif key =="user":
+        name = input("enter name:") 
+        pas = input("enter pasword:")
+        user_list[name] = pas
+        print("user successfully added")
+    else:
+        print("error to add member")
+
 
 while True:
     print("login")
     name = input("enter name:") 
-    pas = input("enter pasword:")    
+    pas = input("enter pasword:")      
+    if admin_list.get(name) == pas:
+        print("login successfully")
+        print("menu \n 1.add new user \n 2.add new admin")
+    elif user_list.get(name) == pas:
+        print("login successfully")
+        print("menu \n 1.show room \n 2.my reserv")
