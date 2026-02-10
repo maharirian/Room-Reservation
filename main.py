1aa917c576aaf3f3700d112d5aa0ca58ecc7403c
mah <ma.haririan1383@gmail.com>
2026-02-10 13:27:06 +0330

    init project

---
+++ main.py
@@ -0,0 +1,10 @@
+room_list = {"room1":"big","room2":"big","room3":"small","room4":"small"}
+reserv_list = []
+admin_list = {"admin":"abcd"}
+user_list = {}
+
+
+while True:
+    print("login")
+    name = input("enter name:") 
+    pas = input("enter pasword:")    
